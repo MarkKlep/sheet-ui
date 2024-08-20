25c888f54bcabbdca9905d94163811f7b5ab8738
Mark Klepanchuk <cheapshoterr@gmail.com>
2024-08-20 18:24:26 +0300

    first commit

---
+++ app/src/App.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className='App'>
+      <table>
+        <tr>
+          <th>Id</th>
+          <th>Name</th>
+          <th>Age</th>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td>John</td>
+          <td>25</td>
+        </tr>
+      </table>
+    </div>
+  );
+}
+
+export default App;
