f08f63006472e53c1d6b0467f88d858a5af37202
mark <cheapshoterr@gmail.com>
2024-08-22 14:44:46 +0300

    clerify code logic

--- app/src/utilities/table-content.ts
+++ app/src/utilities/table-content.ts
@@ -3,16 +3,17 @@ import { COLUMNS, FIRST_N_ROWS } from "../constants";
 export function genTableContent() {
   const table = Array(FIRST_N_ROWS).fill(Array(COLUMNS.length).fill(""));
 
-  const numberedRows = Array(FIRST_N_ROWS)
+  // numberedTable = table + numbered column
+  const numberedTable = Array(FIRST_N_ROWS)
     .fill(null)
     .map((_, index) => [String(index), ...table[index]]);
 
-  numberedRows[0] = [
+  // init data(names of columns) to numberedTable
+  numberedTable[0] = [
     "",
     ...COLUMNS.map((_, index) => String.fromCharCode(65 + index)),
   ];
+  numberedTable[1] = [1, ...COLUMNS];
 
-  numberedRows[1] = [1, ...COLUMNS];
-
-  return numberedRows;
+  return numberedTable;
 }
